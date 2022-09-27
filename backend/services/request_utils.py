89eda1de66baf7f769ec45054aca5941047ec54c
rl8384 <radoslaw.lis@dxc.com>
2022-09-27 20:12:39 +0200

    Add filtering for court

--- backend/services/request_utils.py
+++ backend/services/request_utils.py
@@ -1,22 +1,22 @@
-import functools
 import logging
 
 from rest_framework.response import Response
 from rest_framework import status
-
-from ..models import Rating
-from ..exceptions import TooManyRequestsFromIpException
 
 
 def validate_ip(model):
     """ Check if user with given IP has not rated court before """
     def inner(func):
         def wrapper(*args, **kwargs):
-            ip = get_client_ip(args[1])
+            request = args[1]
+            ip = get_client_ip_from_request(request)
+            court_id = request.data['court']
+            logging.debug(f"Court id = {court_id}")
             logging.debug(f"User IP is {ip}")
-            if model.objects.filter(user_ip=ip).exists():
-                logging.debug("User with this IP has already rated the court")
-                return Response({"detail": f"User with this IP ({ip}) has already rated the court"}, status=status.HTTP_403_FORBIDDEN)
+            kwargs['ip'] = ip
+            if model.objects.filter(user_ip=ip).filter(court=court_id).exists():
+                logging.debug(f"User with this IP ({ip}) has already rated court with id {court_id}")
+                return Response({"detail": f"User with this IP ({ip}) has already rated court with id {court_id}"}, status=status.HTTP_403_FORBIDDEN)
             else:
                 value = func(*args, **kwargs)
                 return value
@@ -24,11 +24,13 @@ def validate_ip(model):
     return inner
 
 
-def get_client_ip(request):
+def get_client_ip_from_request(request):
     logging.debug(f"Request META - {request.META}")
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
+    ip = request.META.get('HTTP_X_REAL_IP')
+    if ip is None:
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
     return ip
