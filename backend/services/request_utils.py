4870da04196e878383bc3787c90a56c4eb3808e8
rl8384 <radoslaw.lis@dxc.com>
2022-09-28 15:37:43 +0200

    Add handling IP exception to validator

--- backend/services/request_utils.py
+++ backend/services/request_utils.py
@@ -5,6 +5,9 @@ import logging
 from rest_framework.response import Response
 from rest_framework import status
 
+from ..models import Court
+from ..exceptions import InvalidRequestException, TooManyRequestsFromIpException
+
 
 def validate_ip(model, validation_type, value):
     """ Check if user with given IP has not rated court before """
@@ -12,16 +15,15 @@ def validate_ip(model, validation_type, value):
         def wrapper(*args, **kwargs):
             request = args[1]
             ip = get_client_ip_from_request(request)
-            validator = IPValidator(model, ip, request.data['court'])
-            validation_result, validation_message = validator.validate(validation_type, value)
-            if validation_result:
-                logging.info(validation_message)
+            try:
+                validator = IPValidator(model, ip, request.data['court'])
+                validator.validate(validation_type, value)
                 kwargs['ip'] = ip
                 response = func(*args, **kwargs)
                 return response
-            else:
-                return Response({"detail": validation_message}, status=status.HTTP_403_FORBIDDEN)
-
+            except (InvalidRequestException, TooManyRequestsFromIpException) as e:
+                logging.info(str(e))
+                return Response({"detail": str(e), "exception": e.__class__.__name__}, status=status.HTTP_403_FORBIDDEN)
         return wrapper
     return inner
 
@@ -65,6 +67,7 @@ class ValidationType(Enum):
 class IPValidator:
     def __init__(self, model, user_ip, court_id):
         logging.info(f"Getting objects of {model} filtering by 'ip' = {user_ip} and 'court_id' = {court_id}")
+        self.validate_if_court_exists(court_id)
         self.user_ip = user_ip
         self.model = model.__name__
         self.court_id = court_id
@@ -74,24 +77,29 @@ class IPValidator:
     def validate(self, validation_type, validation_value):
         logging.info(f"Validating ip based on {validation_type} with validation value =  {str(validation_value)}")
         if not self.objects:
-            return True, f"IP validation successful, no objects created from given IP for given court"
+            logging.info(f"IP validation successful, no objects created from given IP for given court")
         elif validation_type is ValidationType.AMOUNT:
             if len(self.objects) >= validation_value:
-                return False, f"IP validation not successful, total number ({len(self.objects)}) of {self.model} " \
-                              f"objects created in database from ip = {self.user_ip} for court_id = " \
-                              f"{self.court_id} has exceeded or is equal to maximum allowed ({validation_value})"
+                raise TooManyRequestsFromIpException(f"IP validation not successful, total number "
+                                                     f"({len(self.objects)}) of {self.model} objects created in "
+                                                     f"database from ip = {self.user_ip} for court_id = "
+                                                     f"{self.court_id} has exceeded or is equal to maximum "
+                                                     f"allowed ({validation_value})")
             else:
-                return True, f"IP validation successful, {validation_value} > {len(self.objects)}"
+                logging.info(f"IP validation successful, {validation_value} > {len(self.objects)}")
         elif validation_type is ValidationType.TIME_ELAPSED:
             now = datetime.now(timezone.utc)
             difference = now - self.objects[0].creation_date
             if validation_value >= difference:
-                return False, f"IP validation not successful, last created {self.model} object from ip " \
-                              f"{self.user_ip} for court_id = {self.court_id} had been created on " \
-                              f"{self.objects[0].creation_date}, only {difference} time has elapsed till now ({now}) " \
-                              f"and minimum is = {validation_value}"
+                raise TooManyRequestsFromIpException(f"IP validation not successful, last created {self.model} "
+                                                     f"object from ip {self.user_ip} for court_id = {self.court_id} "
+                                                     f"had been created on {self.objects[0].creation_date}, only "
+                                                     f"{difference} time has elapsed till now ({now}) and minimum is "
+                                                     f"= {validation_value}")
             else:
-                return True, f"IP validation successful, {validation_value} >= {now - self.objects[0].creation_date}"
+                logging.info(f"IP validation successful, {validation_value} >= {now - self.objects[0].creation_date}")
 
-
+    def validate_if_court_exists(self, court_id):
+        if not Court.objects.filter(id=court_id).exists():
+            raise InvalidRequestException(f"Court with id = {court_id} does not exist")
 
